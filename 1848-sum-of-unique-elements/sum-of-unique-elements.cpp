03d4756ab53722b7ee8bc80f451787983c4dcbbc
Divanshu Manchanda <90476277+divanshu31@users.noreply.github.com>
2024-03-05 21:15:22 +0530

    Time: 5 ms (25.39%) | Memory: 10.2 MB (14.15%) - LeetSync

---
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        map<int, int> naksha;
+        for(int i = 0; i < nums.size(); i++){
+            naksha[nums[i]]++;
+        }
+        int sum = 0;
+        for(auto i : naksha){
+            if(i.second == 1) sum += i.first;
+        }
+        return sum;
+    }
+};
