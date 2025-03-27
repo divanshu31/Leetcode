e434a695abd31c8b2f88bbe32a5ae8ac46970a6b
Divanshu Manchanda <90476277+divanshu31@users.noreply.github.com>
2025-03-27 10:19:11 +0530

    Time: 47 ms (42.21%) | Memory: 102.9 MB (19.26%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        unordered_map<int, int> mpp, left;
+        for(auto it : nums) mpp[it]++;
+        for(int i = 0; i < nums.size()-1; i++){
+            left[nums[i]]++;
+            mpp[nums[i]]--;
+            if(left[nums[i]] * 2 > (i + 1) && mpp[nums[i]] * 2 > (nums.size()-i-1)){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
