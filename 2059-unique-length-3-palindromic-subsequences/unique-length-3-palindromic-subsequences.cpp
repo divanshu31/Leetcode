92f59cd31622161d9970377fa0366d9d219fe460
Divanshu Manchanda <90476277+divanshu31@users.noreply.github.com>
2025-11-21 07:31:04 +0530

    Time: 15 ms (97.75%) | Memory: 12.6 MB (100.00%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,40 +1,72 @@
+int n;
+int ll[26], rr[26];
 class Solution {
 public:
-    int countPalindromicSubsequence(string inputString) {
-        // Arrays to store the minimum and maximum occurrences of each character in the input string
-        vector<int> minExist(26, INT_MAX);
-        vector<int> maxExist(26, INT_MIN);
-
-        // Populate minExist and maxExist arrays
-        for(int i = 0; i < inputString.size(); i++) {
-            int charIndex = inputString[i] - 'a';
-            minExist[charIndex] = min(minExist[charIndex], i);
-            maxExist[charIndex] = max(maxExist[charIndex], i);
-        }
-
-        // Variable to store the final count of unique palindromic subsequences
-        int uniqueCount = 0;
-
-        // Iterate over each character in the alphabet
-        for (int charIndex = 0; charIndex < 26; charIndex++) {
-            // Check if the character has occurred in the input string
-            if (minExist[charIndex] == INT_MAX || maxExist[charIndex] == INT_MIN) {
-                continue; // No occurrences, move to the next character
+    static inline void find_ll_rr(string& s){
+        fill(ll, ll+26, n);
+        memset(rr, -1, sizeof(rr));
+        
+        for(unsigned i=0; i<n; i++){
+            unsigned x=s[i]-'a';
+            if (ll[x]==n){
+                ll[x]=i;
             }
-
-            // Set to store unique characters between the minimum and maximum occurrences
-            unordered_set<char> uniqueCharsBetween;
-
-            // Iterate over the characters between the minimum and maximum occurrences
-            for (int j = minExist[charIndex] + 1; j < maxExist[charIndex]; j++) {
-                uniqueCharsBetween.insert(inputString[j]);
-            }
-
-            // Add the count of unique characters between the occurrences to the final count
-            uniqueCount += uniqueCharsBetween.size();
+            rr[x]=i;
         }
+    }
 
-        // Return the total count of unique palindromic subsequences
-        return uniqueCount;
+    static inline void find_ll_rr_2(string& s){
+        fill(ll, ll+26, n);
+        memset(rr, -1, sizeof(rr));
+    
+        int seen=0;
+        
+        for(unsigned i=0; i<n && seen<26; i++){
+            unsigned x=s[i]-'a';
+            if (ll[x]==n){
+                ll[x]=i;
+                seen++;
+            }
+        }
+    
+        for(unsigned i=n-1; i>=0 && seen>0; i--){
+            unsigned x=s[i]-'a';
+            if (rr[x]==-1){
+                rr[x]=i;
+                seen--;
+            }
+        }
+    }
+
+    static inline int hasPal3(char x,  string& s, int l, int r){
+        unsigned m;
+        bitset<26> seen=0;
+
+        for(m=l+1; m<r; m++){
+            seen[s[m]-'a']=1;
+        } 
+        return seen.count();
+    }
+
+    static int countPalindromicSubsequence(string& s) {
+        n=s.size();
+        find_ll_rr(s);
+        int count=0;
+    
+        for(char x='a'; x<='z'; x++){
+            int l=ll[x-'a'], r=rr[x-'a'];
+            if (l+1>=r) continue;
+            count+=hasPal3(x, s, l, r);
+        }   
+        return count;
     }
 };
+
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
